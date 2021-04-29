562558b158b55e70035683ea109bfb22f4791d26
Edison Escabarte <edi.escx@gmail.com>
2021-04-29 21:48:19 +0800

    refactor: profile picture, pp size, centered introduction

--- src/app/providers/user.provider.ts
+++ src/app/providers/user.provider.ts
@@ -6,7 +6,7 @@ export const user: User = {
   address: 'Manila, PH',
   email: 'edi.escx@gmail.com',
   mobile_number: '+639083196800',
-  profile_picture: 'assets/images/avatar.jpg',
+  profile_picture: 'https://avatars.githubusercontent.com/u/21971681?s=400&u=6cd26b013c09277e32274b00c8769fc5cfdc27d6&v=4',
   description:
     'Hi! I am a junior mobile application developer enthusiast on building useful applications and UI design, eager to learn and increase my knowledge.',
 };
