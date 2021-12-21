ece1a905836dc7dbc500c5da87d2c017d7520f37
Edison Escabarte <edi.escx@gmail.com>
2021-12-21 14:48:58 +0800

    refactor: add location, update work experience

--- src/app/models/experience.model.ts
+++ src/app/models/experience.model.ts
@@ -3,6 +3,7 @@ import { TimeRange } from './range.model';
 export class Experience extends TimeRange {
   position: string;
   company_name: string;
+  location?: string;
   description?: string;
   responsibilities?: string[];
   type?: 'WORK' | 'EDUCATION';
